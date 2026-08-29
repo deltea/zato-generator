792ba46e5598f6cab7cc095f2440239c126aff64
deltea <thcheetah777@gmail.com>
2026-08-29 17:39:27 +0800

    zato layout

---
+++ src/lib/components/DialogueBox.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  // const TYPING_SPEED = 1.0;
+
+  function closeTab() {
+    window.close();
+  }
+</script>
+
+<div class="border w-full h-64 border-b-0 px-32 pt-16 relative">
+  <p>I'll see where life takes me.</p>
+
+  <div class="flex bottom-0 left-1/2 -translate-x-1/2 absolute text-base text-muted">
+    <button class="px-3 pt-3 hover:text-fg">NEW</button>
+    <button class="px-3 pt-3 hover:text-fg">SAVE</button>
+    <button class="px-3 pt-3 hover:text-fg">SETTINGS</button>
+    <button class="px-3 pt-3 hover:text-fg">SHARE</button>
+    <button class="px-3 pt-3 hover:text-fg" onclick={closeTab}>EXIT</button>
+  </div>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import DialogueBox from "$lib/components/DialogueBox.svelte";
+</script>
+
+<svelte:head>
+  <title>Z.A.T.O. // I Love the Background and Everything On It</title>
+</svelte:head>
+
+<main class="flex flex-col justify-end items-center h-screen px-32">
+  <div class="max-w-7xl w-full">
+    <div class="outline w-full aspect-12/5 bg-[url('/filtered.png')] bg-cover bg-center pixelated border-4 border-bg">
+
+    </div>
+
+    <!-- speaker card -->
+    <div class="text-3xl py-4 px-5 outline w-min -my-6 bg-bg ml-12 z-10 relative">
+      [MARINA]
+    </div>
+
+    <DialogueBox />
+  </div>
+</main>
